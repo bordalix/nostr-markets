e46fc0074ee5caef25b0123b581f915971ef72ae
bordalix <joao.bordalo@gmail.com>
2023-09-27 17:14:09 +0100

    Use shared nostr-utils.js file Remove duplicate markets Add download js file button Show total of markets found

--- js/nostr-markets.js
+++ js/nostr-markets.js
@@ -1,10 +1,7 @@
+let markets
+
 const prettyDate = (unixtime) => {
   return new Date(unixtime * 1000).toDateString()
-}
-
-const marketUrl = (pubkey, tags) => {
-  const stallId = tags.find((tag) => tag[0] === 'd')?.[1]
-  return `https://market.nostr.com/?merchant=${pubkey}&stall=${stallId}#/`
 }
 
 const timeAgo = (time) => {
@@ -19,14 +16,14 @@ const timeAgo = (time) => {
   return 'more then a year ago'
 }
 
-const renderResults = (data) => {
+const renderResults = () => {
   let html = ''
   // sort markets by more recent first
-  const sorted = data.sort((a, b) => b.created_at - a.created_at)
-  for (const { content, created_at, pubkey, tags } of sorted) {
+  const sorted = markets.sort((a, b) => b.created_at - a.created_at)
+  for (const { content, created_at, pubkey, stall_id } of sorted) {
     const { name, description, shipping } = JSON.parse(content)
     // market (aka stall) url
-    const url = marketUrl(pubkey, tags)
+    const url = `https://market.nostr.com/?merchant=${pubkey}&stall=${stall_id}#/`
     // remove id from shipping
     shipping?.forEach((r) => delete r['id'])
     // add market to html
@@ -51,24 +48,34 @@ const renderResults = (data) => {
   $('#results').html(html)
 }
 
+const getMarkets = async () => {
+  const markets = new Map()
+  const filter = { kinds: [30017] }
+  const events = await getEvents(filter)
+  const sorted = events.sort((a, b) => a.created_at - b.created_at)
+  for (const event of sorted) {
+    const { tags } = event
+    const stall_id = tags.find((tag) => tag[0] === 'd')?.[1]
+    if (stall_id) markets.set(stall_id, { ...event, stall_id })
+  }
+  return Array.from(markets.values())
+}
+
 // button click handler
 const fetchMarkets = async () => {
   // reset UI
   $('#fetching-status').html('')
   $('#fetching-progress').css('visibility', 'hidden')
   $('#fetching-progress').val(0)
-  $('#file-download').html('')
-  $('#markets-found').text('')
+  $('#file-download').css('visibility', 'hidden')
+  $('#events-found').text('')
   // messages to show to user
   const checkMark = '&#10003;'
-  const txt = {
-    fetching: 'Fetching from relays... ',
-    download: `Downloading markets file... ${checkMark}`,
-  }
+  const fetching = 'Fetching from relays... '
   // disable button (will be re-enable at the end of the process)
   $('#fetch-markets').prop('disabled', true)
   // inform user that app is fetching from relays
-  $('#fetching-status').html(txt.fetching)
+  $('#fetching-status').html(fetching)
   // show and update fetching progress bar
   $('#fetching-progress').css('visibility', 'visible')
   const fetchInterval = setInterval(() => {
@@ -76,19 +83,20 @@ const fetchMarkets = async () => {
     const currValue = parseInt($('#fetching-progress').val())
     $('#fetching-progress').val(currValue + 1)
   }, 1_000)
-  // get all events from relays
-  const filter = { kinds: [30017] }
-  const markets = await getEvents(filter)
+  // get all markets from relays
+  markets = await getMarkets()
+  $('#markets-found').text(`${markets.length} markets found`)
   // inform user fetching is done
-  $('#fetching-status').html(txt.fetching + checkMark)
+  $('#fetching-status').html(fetching + checkMark)
   // end progress bar
   clearInterval(fetchInterval)
   $('#fetching-progress').val(20)
-  // inform user that markets file (js format) is being downloaded
-  $('#file-download').html(txt.download)
-  // downloadFile(data, 'markets.js')
+  // show download button
+  $('#file-download').css('visibility', 'visible')
   // re-enable fetch markets button
   $('#fetch-markets').prop('disabled', false)
   // render table with markets found
-  renderResults(markets)
+  renderResults()
 }
+
+const download = () => downloadFile(markets, 'markets.js')
